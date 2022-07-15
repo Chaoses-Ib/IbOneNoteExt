ef42c7aa6319aea1db8a122b228b71233ec625a0
Chaoses Ib <Chaos-es@outlook.com>
2022-07-16 03:15:36 +0800

    Add Obsidian-converter

---
+++ Obsidian/Obsidian-converter.py
@@ -0,0 +1,23 @@
+import re
+from pandas.io.clipboard import clipboard_get, clipboard_set
+
+# Copy note from OneNote to Obsidian, and then copy Obsidian
+# Do not copy images
+page = clipboard_get()
+
+# Fix newlines
+page = re.sub('\n\n', '\n', page)
+page = re.sub(r'^#', '\n#', page, flags=re.MULTILINE)
+
+# Fix lists
+page = re.sub(r'^-   ', r'- ', page, flags=re.MULTILINE)
+page = re.sub(r'^(\d)\.  ', r'\1. ', page, flags=re.MULTILINE)
+
+# Titles
+if re.search(r'^## ', page, flags=re.MULTILINE) is None:
+    page = re.sub(r'^#', '', page, flags=re.MULTILINE)
+
+# Time marker
+page = re.sub(r'^t(?:\d{6}|\d{4}|\d{2})$', r'\n<!--\g<0>-->', page, flags=re.MULTILINE)
+
+clipboard_set(page)
